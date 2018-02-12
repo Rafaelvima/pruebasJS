8da8bee6d1da27d21425b291b9fcc4a52076ae41
Rafaelvima <34725153+Rafaelvima@users.noreply.github.com>
2018-02-12 10:33:20 +0100

    mi pagina

---
+++ pag/main.js
@@ -0,0 +1,23 @@
+$(document).ready(main);
+ //ESTE CODIGO LO QUE PERMITE ES QUE EL MENU SE DESPLIEGE CUANDO HAGAMOS CLICK
+var contador = 1;
+ 
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+ 
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+ 
+	});
+ 
+};
